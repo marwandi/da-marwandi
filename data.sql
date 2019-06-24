6914efa80335c4fbec6177f93a3b33c96e63b6f5
marwandi <50399709+marwandi@users.noreply.github.com>
2019-06-24 12:00:45 +0700

    Add files via upload

---
+++ data.sql
@@ -0,0 +1,8 @@
+insert into Plan(id_plan, name, price, description, min_age, min_income)
+values (1, 'PlanA', 500000, 'Kamar Rawat Inap = 500.000/hari, Biaya Perawatan Rawat Inap = 75.000.000/Tahun, Biaya Rawat Jalan = 75.000.000/Tahun', 17, 5000000),
+       (2, 'PlanB', 700000, 'Kamar Rawat Inap = 750.000/hari, Biaya Perawatan Rawat Inap = 100.000.000/Tahun, Biaya Rawat Jalan = 100.000.000/Tahun', 35, 7000000),
+       (3, 'PlanC', 1000000, 'Kamar Rawat Inap = 1000.000/hari, Biaya Perawatan Rawat Inap = 120.000.000/Tahun, Biaya Rawat Jalan = 120.000.000/Tahun', 55, 10000000),
+
+insert into User(id_user, name, sex, birth_date, income, phone_number, job_type, smoking_status, plan)
+values (1, 'marwan', 'M', '1982-02-02', 5000000, '+62821321321', '1', 1, 1),
+       (2, 'marvel denver', 'F', '1981-01-01', 5000000, 4324432334, 2, '0', 2);
